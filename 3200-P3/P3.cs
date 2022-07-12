ee32ccae45c1ec7842e71fdb6fbe02a7002d56ed
gnoahh <44447075+gnoahh@users.noreply.github.com>
2022-07-11 18:24:12 -0700

    Add files via upload

---
+++ 3200-P3/P3.cs
@@ -0,0 +1,302 @@
+﻿/* Hoang Do
+ * P3.cs
+ * 05/04/2021
+ * Driver class to test the dataLine, dataMirror and dataInsert objects
+ * This class will call 4 other methods (createObject, testdataLine, testdataMirror, testdataInsert) to conduct testing 
+ * 
+ * 
+ */
+using System;
+
+namespace Project3
+{
+    class Program
+    {
+        private const int NUM_TEST = 3;
+        private const int TYPE = 3;
+        private const int INVALID = -1;
+        private const int MAX_SIZE = NUM_TEST * TYPE;
+        private const int RANDOM_CAP = 10;
+        private const int RANCOM_MAX = 200;
+        private const int RANDOM_MIN = 1;
+        public static void Main(string[] args)
+        {
+
+            Random rand = new Random();
+            dataLine[] arrObj = new dataLine[MAX_SIZE];
+            createObjects(rand, arrObj);
+
+            Console.WriteLine(">>>Test dataLine objects: ");
+            testDataline(arrObj);
+            Console.WriteLine();
+
+            Console.WriteLine(">>>Test dataMirror objects: ");
+            testDataMirror(arrObj);
+            Console.WriteLine();
+
+            Console.WriteLine(">>>Test dataInsert objects: ");
+            testDataInsert(arrObj);
+            Console.WriteLine();
+
+
+            
+           
+        }
+        //Create a heterogenous collection
+        private static void createObjects(Random rand, dataLine[] arrObj)
+        {
+            int[] tempArr;
+            for(int i = 0; i < NUM_TEST; i++)
+            {
+                tempArr = new int[rand.Next(RANDOM_CAP)];
+                for(int j = 0; j < tempArr.Length; j++)
+                {
+                    tempArr[j] = rand.Next(RANDOM_MIN, RANCOM_MAX); 
+                }
+                arrObj[i] = new dataLine(tempArr);
+            }
+
+            for (int i = NUM_TEST; i < NUM_TEST * 2; i++)
+            {
+                tempArr = new int[rand.Next(RANDOM_CAP)];
+                for (int j = 0; j < tempArr.Length; j++)
+                {
+                    tempArr[j] = rand.Next(RANDOM_MIN, RANCOM_MAX);
+                }
+                arrObj[i] = new dataMirror(tempArr);
+            }
+
+            for (int i = NUM_TEST * 2; i < MAX_SIZE; i++)
+            {
+                tempArr = new int[rand.Next(RANDOM_CAP)];
+                for (int j = 0; j < tempArr.Length; j++)
+                {
+                    tempArr[j] = rand.Next(RANDOM_MIN, RANCOM_MAX);
+                }
+                arrObj[i] = new dataInsert(tempArr);
+            }
+
+            
+
+
+        }
+
+        //test dataLine objects
+        private static void testDataline(dataLine[] arrObj)
+        {
+            int[] tempArr;
+            for(int i = 0; i < NUM_TEST; i++)
+            {
+                tempArr = arrObj[i].retrieve(NUM_TEST);
+                foreach(int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Passing in invalide y now: ");
+            for (int i = 0; i < NUM_TEST; i++)
+            {
+                tempArr = arrObj[i].retrieve(INVALID);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Attempt to retrieve inactive objects: ");
+            for (int i = 0; i < NUM_TEST; i++)
+            {
+                tempArr = arrObj[i].retrieve(NUM_TEST);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("After resetting Dataline objects: ");
+            for (int i = 0; i < NUM_TEST; i++)
+            {
+                arrObj[i].reset();
+                tempArr = arrObj[i].retrieve(NUM_TEST);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Test query method in dataLine objects: ");
+            int placeHolder; 
+            for (int i = 0; i < NUM_TEST; i++)
+            {
+                placeHolder = arrObj[i].getElement();
+                if (arrObj[i].query(placeHolder))
+                {
+                    Console.WriteLine(placeHolder + " exists in the encapsulated array!");
+                }
+                else
+                {
+                    Console.WriteLine(placeHolder + " doesn't exist in the encapsulated array!");
+                }
+            }
+
+        }
+
+        //Test dataMirror objects
+        private static void testDataMirror(dataLine[] arrObj)
+        {
+            int[] tempArr;
+
+            for (int i = NUM_TEST; i < NUM_TEST*2; i++)
+            {
+                tempArr = arrObj[i].retrieve(NUM_TEST);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Test passing in invalid y to dataMirror objects: ");
+            for (int i = NUM_TEST; i < NUM_TEST * 2; i++)
+            {
+                tempArr = arrObj[i].retrieve(INVALID);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Attempt to retrieve inactive dataMirror objects: ");
+            for (int i = NUM_TEST; i < NUM_TEST * 2; i++)
+            {
+                tempArr = arrObj[i].retrieve(INVALID);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("After resetting dataMirror objects: ");
+            for (int i = NUM_TEST; i < NUM_TEST * 2; i++)
+            {
+                arrObj[i].reset();
+                tempArr = arrObj[i].retrieve(NUM_TEST);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Attempting to make the first object permanently deactivated: ");
+            for (int i = NUM_TEST; i < NUM_TEST * 2; i++)
+            {
+                tempArr = arrObj[i].retrieve(INVALID);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("The first object is now permanently deactivated: ");
+            for (int i = NUM_TEST; i < NUM_TEST * 2; i++)
+            {
+                arrObj[i].reset();
+                tempArr = arrObj[i].retrieve(NUM_TEST);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Attempt to call query method on a permanently dead object: ");
+            int placeHolder = arrObj[NUM_TEST].getElement();
+            if (arrObj[NUM_TEST].query(placeHolder))
+            {
+                Console.WriteLine(placeHolder + " exists in the encapsulated array!");
+            }
+            else
+            {
+                Console.WriteLine(placeHolder + " doesn't exist in the encapsulated array!");
+            }
+            
+
+        }
+
+        //test dataInsert objects
+        private static void testDataInsert(dataLine[] arrObj)
+        {
+            int[] tempArr;
+
+            for (int i = NUM_TEST * 2; i < MAX_SIZE; i++)
+            {
+                tempArr = arrObj[i].retrieve(NUM_TEST);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Call each dataInsert object again to make the largest number decremented");
+            Console.WriteLine("First object to have k replace a: ");
+            for (int i = NUM_TEST * 2; i < MAX_SIZE; i++)
+            {
+                tempArr = arrObj[i].retrieve(NUM_TEST);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Call each dataInsert object again to make the largest number decremented");
+            Console.WriteLine("Second object to have k replace a: ");
+            for (int i = NUM_TEST * 2; i < MAX_SIZE; i++)
+            {
+                tempArr = arrObj[i].retrieve(NUM_TEST);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Test passing in invalid y: ");
+            for (int i = NUM_TEST * 2; i < MAX_SIZE; i++)
+            {
+                tempArr = arrObj[i].retrieve(INVALID);
+                foreach (int j in tempArr)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Test query method in inactive dataInsert objects:");
+            int placeHolder;
+            for (int i = NUM_TEST * 2; i < MAX_SIZE; i++)
+            {
+                placeHolder = arrObj[i].getElement();
+                if (arrObj[i].query(placeHolder))
+                {
+                    Console.WriteLine(placeHolder + " exists in the encapsulated array!");
+                }
+                else
+                {
+                    Console.WriteLine(placeHolder + " doesn't exist in the encapsulated array!");
+                }
+            }
+
+
+
+        }
+
+
+    }
+}
---
+++ 3200-P3/dataInsert.cs
@@ -0,0 +1,175 @@
+﻿/* Hoang Do
+ * dataInsert.cs
+ * 05/04/2021
+ * Description: dataInsert class is a child class of the dataLine class
+ * dataInsert object supports functionality of xax
+ * upon kth query, k replaces a
+ * every query decrements the largest value in xax
+ * 
+ * Interface Invariants:
+ * query only yields true if object is still active. If not, always returns false
+ * retrieve only yields a snippet of xax array only if the object is still active.
+ * If not, return an array of -1
+ * 
+ * In the constructor, if the passed-in array has a length of 0, a default array will be
+ * used as the encapsulated array
+ * 
+ * The client can use the reset method (from the parent) to reactivate the object when it's inactive
+ * 
+ * Class Invariants:
+ * dataInsert has its own encapsulated array called xax. It has a length of x * 2 + 1. 
+ * static objCount to update whenever new dataInsert created and to determine the bound
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Project3
+{
+    public class dataInsert:dataLine
+    {
+        protected int[] xax;
+        protected int queryCount;
+        protected int bound;
+        protected static int objCount = 0;
+
+
+        public dataInsert(int[] input):base(input)
+        {
+            objCount++;
+            bound = objCount;
+            queryCount = 0;
+            xax = new int[x.Length * 2 + 1];
+            addInsert();
+        }
+
+        //helper method to used in the constructor only
+        private void addInsert()
+        {
+            int accumulator = bound;
+            for (int i = 0; i < x.Length; i++)
+            {
+                accumulator += x[i];
+
+                xax[x.Length] = accumulator;
+            }
+            for (int i = 0; i < x.Length; i++)
+            {
+                xax[i] = x[i];
+                xax[x.Length + i + 1] = x[i];
+            }
+        }
+
+        //helper method to help update the query and change a when needed
+        private void queryUpdate()
+        {
+            queryCount++;
+            if(queryCount > bound)
+            {
+                xax[x.Length] = bound;
+            }
+
+        }
+
+        public override int[] retrieve(int y)
+        {
+            queryUpdate();
+            updateMax();
+            if (!active)
+            {
+                return new int[SIZE_SINGLE] { -1 };
+            }
+
+            if(y > xax.Length || y < 0)
+            {
+                active = false;
+                return new int[SIZE_SINGLE] { -1 };
+            }
+
+
+            return getMiddle(y);
+        }
+
+        private int[] getMiddle(int y)
+        {
+            int middleIndex = xax.Length / 2 - 1;
+            int[] resultArr = new int[y];
+            for (int i = 0; i < y; i++)
+            {
+                resultArr[i] = xax[middleIndex + i];
+            }
+
+            return resultArr;
+        }
+
+        //private utility method to update the maximas
+        private void updateMax()
+        {
+            int max = xax[0];
+            List<int> maxList = new List<int>();
+            for (int i = 0; i < xax.Length; i++)
+            {
+                if (max < xax[i])
+                {
+                    max = xax[i];
+                }
+            }
+
+            for (int i = 0; i < xax.Length; i++)
+            {
+                if (max == xax[i])
+                {
+                    maxList.Add(i);
+                }
+            }
+
+            foreach(int i in maxList)
+            {
+                xax[i]--;
+            }
+        }
+        public override bool query(int y)
+        {
+            queryUpdate();
+            updateMax();
+            if (y < 0)
+            {
+                active = false;
+                return false;
+            }
+
+            if (!active)
+            {
+                return false;
+            }
+
+            return base.query(y);
+        }
+
+        public override int getElement()
+        {
+            return xax[xax.Length/2];
+        }
+    }
+}
+
+/* Implementation invariants:
+ * xax has the length of x * 2 + 1 to account for the second half of x and a
+ * 
+ * dataInsert object has queryCount to keep tracks of how many times query(y) and
+ * retrieve(y) are called. Whenever they are called, the largest element in the xax array
+ * will decrement by 1, the queryCount will increase by 1. Once the queryCount is larger 
+ * than the object bound, k will replace a
+ * 
+ * The bound is determined by how many object of dataInsert class there are. 
+ * 
+ * getElement will return the middle element of xax for client's testing the query method only
+ * 
+ * 
+ * upDateMax is to find the maximum then decrement it. Since there may be more than one max
+ * (duplicate numbers), a list is used to capture the indexes of the maximas then decrement. 
+ * 
+ * a is calculated as the sum of all the elements in x plus the bound
+ * a = sum x + bound
+ */
